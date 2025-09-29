fdb9114703a279fcae33a4c84f9bf6bfb9a86768
Divyanshu <divyanshu473b@gmail.com>
2025-09-29 23:51:31 +0530

    feat(ui) add custom colors and themes to button component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,50 @@
-import Button from "./components/Button"
-
+import Button from "./components/Button";
 
 const App = () => {
   return (
-    <Button text={"Hello"} size="sm" variant={"primary"} onClick={() => {console.log("Hello")}}/>
-  )
-}
+    <div className="flex items-center gap-8 mt-2">
+      <Button
+        text={"Size: sm"}
+        size="sm"
+        variant={"primary"}
+        onClick={() => {
+          alert("Hello");
+        }}
+      />
+      <Button
+        text={"Size: md"}
+        size="md"
+        variant={"primary"}
+        onClick={() => {
+          alert("Hello");
+        }}
+      />
+      <Button
+        text={"Size: lg"}
+        size="lg"
+        variant={"primary"}
+        onClick={() => {
+          alert("Hello");
+        }}
+      />
+      <Button
+        text={"Size: xl"}
+        size="xl"
+        variant={"primary"}
+        onClick={() => {
+          alert("Hello");
+        }}
+      />
+      <Button
+        text={"Size: 2xl"}
+        size="2xl"
+        variant={"secondary"}
+        onClick={() => {
+          alert("Hello");
+        }}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- Frontend/src/components/Button.tsx
+++ Frontend/src/components/Button.tsx
@@ -7,13 +7,29 @@ interface ButtonUI {
   onClick: () => void;
 }
 
-const buttonVariants = {
-  primary: "bg-purple-600",
-  secondary: "bg-purple-300",
+const sizeStyles = {
+  sm: "w-24 py-1 px-2 rounded-xl",
+  md: "w-fit py-2 px-6 rounded-xl",
+  lg: "w-fit py-2 px-12 rounded-xl",
+  xl: "w-fit py-2 px-14 rounded-xl",
+  "2xl": "w-fit py-2 px-16 rounded-xl",
+};
+
+const variantStyles = {
+  primary: "bg-btnPrimary",
+  secondary: "bg-btnSecondary",
 };
 
 const Button = (props: ButtonUI) => {
-  return <div className={buttonVariants[props.variant]}>{props.text}</div>;
+  console.log((variantStyles[props.variant], sizeStyles[props.size]));
+  return (
+    <div
+      onClick={props.onClick}
+      className={`${variantStyles[props.variant]} ${sizeStyles[props.size]}`}
+    >
+      {props.text}
+    </div>
+  );
 };
 
 export default Button;
